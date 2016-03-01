524eec1c85c3225fc50ad3c4f0e9703d81fe5c29
Carol <carolngu@gmail.com>
2016-02-29 20:11:02 -0800

    Checkpoint/Assignment 21 Reading CSV: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -81,6 +81,32 @@ RSpec.describe AddressBook do
       entry_five = book.entries[4]
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
+  end
 
+  describe "#import_from_csv2" do
+    it "imports the entries from second csv file" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+
+    it "imports 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Ken", "777-567-8979", "ken@blocmail.com")
+    end
+
+    it "imports 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Kris", "777-567-2234", "kris@blocmail.com")
+    end
+
+    it "imports 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Richard", "777-567-4532", "richard@blocmail.com")
+    end
   end
 end
